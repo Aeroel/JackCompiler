783d9322b7710ce18aaa02dd739ab8ae7f21d4f4
Aeroel <hmm>
2025-04-22 11:28:46 +0300

    scaffolding for rewriting into TS

--- Config.js
+++
@@ -1,7 +0,0 @@
-export { Config };
-class Config {
-    static whatOSDoYouUse = "windows";
-    static set_new_line_type(type) {
-        this.new_line_type = type;
-    }
-}
--- Helper_Functions.js
+++
@@ -1,9 +0,0 @@
-export { Helper_Functions };
-class Helper_Functions {
-    static newlines = {
-        linux: '\n',
-        }
-    static getNewline() {
-       return this.newlines.linux;
-    }
-}
